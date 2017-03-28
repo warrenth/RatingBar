d7633b8950bd39e557bfa81f4ea41e6c505966b9
김태훈(152317) <152317@emart.com>
2017-03-28 14:24:30 +0900

    init project

---
+++ app/src/main/java/pe/warrneth/ratingbarsample/MainActivity.java
@@ -0,0 +1,18 @@
+package pe.warrneth.ratingbarsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.warrneth.ratingbar.RatingBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RatingBar ratingBar = new RatingBar(this);
+
+    }
+}
---
+++ ratingbar/src/main/java/pe/warrneth/ratingbar/RatingBar.java
@@ -0,0 +1,21 @@
+package pe.warrneth.ratingbar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by 152317 on 2017-03-28.
+ */
+
+public class RatingBar extends View {
+
+    public RatingBar(Context context) {
+        super(context);
+    }
+
+    public RatingBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
